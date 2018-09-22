0ec305df4290c00e7774f72b5d4098f64448ed91
Suhel <Suhel@iMac-O-Suhel.local>
2018-09-22 18:39:31 +0530

    updated socket code

--- server/server.js
+++ server/server.js
@@ -1,15 +1,34 @@
 const path = require("path");
 const publicpath =  path.join(__dirname,"../public");
 const express = require("express");
+const http =require("http");
+const socketio = require("socket.io");
 
 console.log(publicpath);
 console.log(__dirname+"/../public");
 
-var port =  process.env.PORT;
+var port =  process.env.PORT || 3000;
+
 var app = express();
 app.use(express.static(publicpath));
 
-app.listen(port,()=>{
+var server =  http.createServer(app);
+var io =  socketio(server);
+
+io.on("connection",(socket)=>{
+    console.log("Connected to client");
+
+    socket.on("disconnect",()=>{
+        console.log("Client is disconnected");
+    });
+
+});
+
+// app.listen(port,()=>{
+//     console.log(`connection is up on port ${port}`);
+// });
+
+server.listen(port,()=>{
     console.log(`connection is up on port ${port}`);
 });
 
